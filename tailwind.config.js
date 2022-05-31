55349839fe626579f87cf65db1e3c86684c408a8
PassyahRaffi <passyah11@gmail.com>
2022-05-31 13:08:41 +0700

    backup file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        almarai: ['Almarai', 'sans-serif'],
+        notoNaskhArabic: ['Noto Naskh Arabic', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
